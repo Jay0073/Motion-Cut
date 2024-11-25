735e507ab04fa9d1ed7b67fcab82ca1d6502f9aa
Jayendra <voutlajay8765@gmail.com>
2024-11-25 20:07:38 +0530

    added product container

---
+++ E-commerce Product Page/script.js
@@ -0,0 +1,54 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+const totalSlides = slides.length;
+
+function showSlide(index) {
+  if (index >= totalSlides) {
+    currentSlide = 0;
+  } else if (index < 0) {
+    currentSlide = totalSlides - 1;
+  } else {
+    currentSlide = index;
+  }
+  
+  const slider = document.getElementById("slider");
+  slider.style.transform = `translateX(-${currentSlide * 100}%)`;
+
+}
+
+function nextSlide() {
+  showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+  showSlide(currentSlide - 1);
+}
+
+// Initialize the slider
+showSlide(currentSlide);
+
+const element = document.querySelector(".container");
+
+let startX;
+let endX;
+
+element.addEventListener("touchstart", (event) => {
+  startX = event.touches[0].clientX;
+});
+
+element.addEventListener("touchend", (event) => {
+  endX = event.changedTouches[0].clientX;
+
+  const distance = endX - startX;
+  const threshold = 50; // Adjust the threshold as needed
+
+  if (Math.abs(distance) > threshold) {
+    if (distance > 0) {
+      // Swipe right
+      prevSlide();
+    } else {
+      // Swipe left
+      nextSlide();
+    }
+  }
+});
